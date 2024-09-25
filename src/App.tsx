f1d5d5c35518b3c1cce09210c4bc7aaaf62f4238
Ma5ha-ZenDev <103930787+Ma5ha-ZenDev@users.noreply.github.com>
2024-09-25 07:58:15 +0200

    test case

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { createQuery } from "react-query-kit";
+import axios from "axios";
 
-function App() {
-  const [count, setCount] = useState(0)
+const delay = (ms = 300) => {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+};
+
+const fetchData = async () => {
+  const { data } = await delay(1000).then(() =>
+    axios.get("https://jsonplaceholder.typicode.com/todos/1")
+  );
+  return data;
+};
+
+const useTestQuery = createQuery({ queryKey: ["test"], fetcher: fetchData });
+
+const App = () => {
+  const { data } = useTestQuery({ enabled: false });
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <h1>Test Data:</h1>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+    </div>
+  );
+};
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { StrictMode, Suspense } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
+const queryClient = new QueryClient({
+  //@ts-ignore
+  defaultOptions: { queries: { suspense: true } },
+});
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <QueryClientProvider client={queryClient}>
+      <Suspense fallback={<h1>Loading....</h1>}>
+        <App />
+      </Suspense>
+
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  </StrictMode>
+);
